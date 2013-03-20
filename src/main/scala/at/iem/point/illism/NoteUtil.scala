5c742586c0a3dc10ae0e7cb918252babba4e1039
Sciss <contact@sciss.de>
2013-03-20 17:45:30 +0100

    begin work on splitMelodicHarmonic. returns something, but seems to be overly inclusive with respect to chords?

--- src/main/scala/at/iem/point/illism/NoteUtil.scala
+++ src/main/scala/at/iem/point/illism/NoteUtil.scala
@@ -28,6 +28,7 @@ package at.iem.point.illism
 import de.sciss.midi
 import midi.{Track, TickRate}
 import collection.immutable.{IndexedSeq => IIdxSeq}
+import annotation.tailrec
 
 object NoteUtil {
 //  def findMonophones(in: IIdxSeq[OffsetNote], overlapTolerance: Double = 0.1): IIdxSeq[IIdxSeq[OffsetNote]] = {
@@ -74,5 +75,92 @@ object NoteUtil {
 
   def stabbings(notes: IIdxSeq[OffsetNote]): IIdxSeq[Double] = {
     notes.flatMap(n => n.offset :: n.stop :: Nil).toSet.toIndexedSeq.sorted
+  }
+
+  def splitMelodicHarmonic(notes: IIdxSeq[OffsetNote], minChordDuration: Double = 0.1, minSeq: Int = 2):
+    (IIdxSeq[((Double, Double), Vector[OffsetNote])], IIdxSeq[((Double, Double), Vector[Chord])]) = {
+
+    var resSeq      = Vector.empty[((Double, Double), Vector[OffsetNote])]
+    var resChords   = Vector.empty[((Double, Double), Vector[Chord])]
+    if (notes.isEmpty) return (resSeq, resChords)
+
+    // the cleaning is only used for generating the stabbing points
+    val tFlt    = NoteUtil.clean(notes, minDuration = minChordDuration)
+    val stabs   = NoteUtil.stabbings(tFlt)
+    var chords  = Vector.empty[Chord]
+    val pairs   = stabs.sliding(2, 1)
+
+    // initially put all notes into the sequential bin, then
+    // as chords are detected remove them from this set
+    var seqSet  = Set(notes:_ *)
+
+    pairs.foreach {
+      case IIdxSeq(start, stop) =>
+        val par = notes.filter { n =>
+          n.offset - minChordDuration <= start && n.stop + minChordDuration >= stop
+        }
+        if (par.size >= 2) {
+          val segm = par.map { n0 =>
+            val n1 = if (start - n0.offset > minChordDuration) n0.replaceStart(start) else n0
+            val n2 = if (n1.stop - stop    > minChordDuration) n1.replaceStop (stop ) else n1
+            n2
+          }
+          chords :+= Chord(segm.sortBy(_.pitch))
+          seqSet --= par
+        }
+    }
+
+    val seq   = seqSet.to[Vector].sortBy(_.offset)
+    chords    = chords.sortBy(_.minOffset)
+
+    var seqIdx      = 0
+    var chordIdx    = 0
+    var seqSpan     = Vector.empty[OffsetNote]
+    var chordSpan   = Vector.empty[Chord]
+
+    def isExhausted = seqIdx == seq.size || chordIdx == chords.size
+    def isHoriz     = seq(seqIdx).offset < chords(chordIdx).minOffset
+
+    def flush() {
+      if (seqSpan.nonEmpty) {
+        resSeq :+= ((seqSpan.head.offset, seqSpan.map(_.stop).max), seqSpan)
+        seqSpan  = Vector.empty
+      }
+      if (chordSpan.nonEmpty) {
+        resChords :+= ((chordSpan.head.minOffset, chordSpan.map(_.maxStop).max), chordSpan)
+        chordSpan   = Vector.empty
+      }
+    }
+
+    @tailrec def horizontal() {
+      val n     = seq(seqIdx)
+      seqIdx   += 1
+      seqSpan :+= n
+      if (!isExhausted) {
+        if (isHoriz) horizontal()
+        else {
+          flush()
+          vertical()
+        }
+      }
+    }
+
+    @tailrec def vertical() {
+      val n       = chords(chordIdx)
+      chordIdx   += 1
+      chordSpan :+= n
+      if (!isExhausted) {
+        if (!isHoriz) vertical()
+        else {
+          flush()
+          horizontal()
+        }
+      }
+    }
+
+    if (!isExhausted) { if (isHoriz) horizontal() else vertical() }
+    flush()
+
+    (resSeq, resChords)
   }
 }
